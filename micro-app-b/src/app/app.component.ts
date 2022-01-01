b8ed7726ddfdd2b3aaef96aad52b1586cb48b332
pranesh229 <pranesh229@gmail.com>
2022-01-01 16:58:53 -0500

    Fixing routing issue

--- micro-app-b/src/app/app.component.ts
+++ micro-app-b/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { SubscriptionLike } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'micro-app-b';
-  constructor(private router: Router) {
-    this.router.initialNavigation();
+  locationSub!: SubscriptionLike;
+  constructor(private router: Router, private location: Location) {
+
+    this.locationSub = this.location.subscribe(() => {
+      this.router.navigateByUrl(this.location.path(true));
+    })
+    // this.router.initialNavigation();
+    this.router.navigateByUrl(this.location.path(true));
+  }
+  ngOnDestroy(): void {
+    this.locationSub.unsubscribe();
+    this.router.dispose();
   }
 }
